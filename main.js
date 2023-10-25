bc2c9a975154c09c8ffbd4ab2b4c5fe1a2fe0e99
EnzoMinoru <134522847+EnzoMinoru@users.noreply.github.com>
2023-10-25 11:43:30 -0300

    gg

--- main.js
+++ main.js
@@ -1,5 +1,47 @@
 alert('hello wrld')
 function tocasompom(){
-    document.querySelection('#som_tecla_pom')
+    document.querySelection('#som_tecla_pom').play();
 }
-document,querySelector('tecla_pom'),onclick =
+document,querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomclap(){
+    document.querySelection('#som_tecla_clap').play();
+}
+document,querySelector('.tecla_clap').onclick = tocaSomclap;
+
+function tocaSomtim(){
+    document.querySelection('#som_tecla_tim').play();
+}
+document,querySelector('.tecla_tim').onclick = tocaSomtim;
+
+function tocaSompuff(){
+    document.querySelection('#som_tecla_puff').play();
+}
+document,querySelector('.tecla_puff').onclick = tocaSompuff;
+
+function tocaSomsplash(){
+    document.querySelection('#som_tecla_splash').play();
+}
+document,querySelector('.tecla_splash').onclick = tocaSomsplash;
+
+function tocasomtoim(){
+    document.querySelection('#som_tecla_toim').play();
+}
+document,querySelector('.tecla_toim').onclick = tocaSomtoim;
+
+function tocasompsh(){
+    document.querySelection('#som_tecla_psh').play();
+}
+document,querySelector('.tecla_psh').onclick = tocasompsh;
+
+function tocasomtic(){
+    document.querySelection('#som_tecla_tic').play();
+}
+document,querySelector('.tecla_tic').onclick = tocaSomtic;
+
+function tocasomtom(){
+    document.querySelection('#som_tecla_tom').play();
+}
+document,querySelector('.tecla_tom').onclick = tocaSomtom;
+
+
