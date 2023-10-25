86e13b00229100d873d43aa9db5d967507dc3136
EnzoMinoru <134522847+EnzoMinoru@users.noreply.github.com>
2023-10-25 09:09:42 -0300

    assss

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 alert('hello wrld')
-function tocasompom()
-document.querySelection('#som_tecla_pom')
+function tocasompom(){
+    document.querySelection('#som_tecla_pom')
+}
+document,querySelector('tecla_pom'),onclick =
